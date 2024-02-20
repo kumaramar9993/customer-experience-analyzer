879e90629d56d55b619a3057ae8467094c6722b6
Amar Kumar <kumaramar9993@gmail.com>
2024-02-20 18:02:27 +0530

    Update tagging.py

--- gptcore/tagging.py
+++ gptcore/tagging.py
@@ -19,74 +19,7 @@ class AutoTag():
                     print(e)
                     pass
         return tagged_responses
-    def write_reviews():
-        
-
-def __init__(self, ref_path, text_col, sample_sheet='0', keyword_expansions='keyword_expansions',
-                 custom_afterprompt='\n\nEach Review can mention multiple {}.\n\n', titles_col=False):
-        self.ref_path = ref_path
-        self.text_col = text_col
-        self.sample_sheet_name = sample_sheet
-        self.kw_expansions_name = keyword_expansions
-        self.sample_sheet = pd.read_excel(ref_path, sheet_name=sample_sheet)
-        self.keyword_expansions = pd.read_excel(ref_path, sheet_name=keyword_expansions
-                                                ).set_index('keyword')['expansions'].str.split(',').apply(
-            lambda x: [r.strip() for r in x]).to_dict()
-        xls = pd.ExcelFile(ref_path)
-        self.task_names = xls.sheet_names[2:]
-        print(self.task_names)
-        self.descriptions = dict(map(lambda x: (x, pd.read_excel(ref_path, sheet_name=x).columns[1]), self.task_names))
-        self.includes = dict(
-            map(lambda x: (x, {k: v for k, v in zip(pd.read_excel(ref_path, sheet_name=x, skiprows=[0])[x],
-                                                    [[e.strip() for e in cl if e != ''] for cl in
-                                                     pd.read_excel(ref_path, sheet_name=x, skiprows=[0])[
-                                                         'Include_Keywords']
-                                                    .fillna('').str.split(',')])}), self.task_names))
-        self.excludes = dict(
-            map(lambda x: (x, {k: v for k, v in zip(pd.read_excel(ref_path, sheet_name=x, skiprows=[0])[x],
-                                                    [[e.strip() for e in cl if e != ''] for cl in
-                                                     pd.read_excel(ref_path, sheet_name=x, skiprows=[0])[
-                                                         'Exclude_Keywords']
-                                                    .fillna('').str.split(',')])}), self.task_names))
-        self.exclude_specifics = dict(
-            map(lambda x: (x, {k: v for k, v in zip(pd.read_excel(ref_path, sheet_name=x, skiprows=[0])[x],
-                                                    [[e.strip() for e in cl if e != ''] for cl in
-                                                     pd.read_excel(ref_path, sheet_name=x, skiprows=[0])[
-                                                         'Exclude_keywords_specific_applied_to']
-                                                    .fillna('').str.split(',')])}), self.task_names))
-
-        self.tasks = dict(map(lambda x: (x, pd.read_excel(ref_path, sheet_name=x, skiprows=[0])), self.task_names))
-        self.prompts = dict(map(lambda x: (x,
-                                           f'{self.descriptions[x].strip()} The {x} fall into {len(self.tasks[x])} categories:\n' + '\n'.join(
-                                               map(
-                                                   lambda y: f'{y[0] + 1}. {y[1][1][0]} - {y[1][1][1]}',
-                                                   enumerate(self.tasks[x].iterrows()))) + custom_afterprompt.format(
-                                               x)), self.task_names))
-    
-
-def get_completion(prompt, model="gpt-3.5-turbo"): # Andrew mentioned that the prompt/ completion paradigm is preferable for this class
-    messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0, # this is the degree of randomness of the model's output
-    )
-    return response.choices[0].message["content"]
-
-def write_json(response,output_file):
-    with open(output_file, "w") as file:
-        json.dump(response, file, indent=4)
-    print("JSON list has been written to", output_file)
-
-
-
-def json_to_df(input_file,output_file):
-    with open(input_file, "rb") as file:
-        responses = json.loads(file.read())
-    
-    results = []
-    for response in responses:
-        results.append(json.loads(response))
-    
-    pd.json_normalize(results).to_csv(output_file,index=False)
-
+    def write_tagged_responses(tagged_responses_file):
+        write_json(self.tagged_responses,tagged_responses_file)
+    def write_tagged_responses(input_json_path,output_csv_path):
+        json_to_df(input_json_path,output_csv_path)
